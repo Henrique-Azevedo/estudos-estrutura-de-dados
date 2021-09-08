a1ea151e9ecf439366a1f2f6b7d96d2ca0e524b1
Henrique-Azevedo <henrique.azevedo.pro@gmail.com>
2021-09-08 05:45:36 -0300

    first commit

---
+++ src/com/atribuicao/referencia/Main.java
@@ -0,0 +1,23 @@
+package com.atribuicao.referencia;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    int intA = 1;
+    int intB = intA;
+
+    System.out.println("IntA=" + intA + " IntB=" + intB);
+    intA = 2;
+    System.out.println("IntA=" + intA + " IntB=" + intB);
+
+    MeuObj objA = new MeuObj(1);
+    MeuObj objB = objA;
+
+    System.out.println("objA=" + objA + " objB=" + objB);
+    objA.setNum(2);
+    System.out.println("objA=" + objA + " objB=" + objB);
+
+    }
+
+}
---
+++ src/com/no/encadeamento/No.java
@@ -0,0 +1,39 @@
+package com.no.encadeamento;
+
+public class    No {
+
+    public String conteudo;
+    public No proximoNo;
+
+    public No(String conteudo) {
+        this.proximoNo = null;
+        this.conteudo = conteudo;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public No getProximoNo() {
+        return proximoNo;
+    }
+
+    public void setProximoNo(No proximoNo) {
+        this.proximoNo = proximoNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "conteudo='" + conteudo + '\'' +
+                '}';
+    }
+}
+
+
+
+
